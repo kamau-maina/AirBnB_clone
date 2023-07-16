b9dfbee22678e7a9b6004e7be36e47d5dc648214
Jane Fadhili Njaga <janefadhili@gmail.com>
2023-07-16 10:53:55 -0700

    changes to incorporate the added classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -9,6 +9,12 @@ import uuid
 import os
 from models.base_model import BaseModel
 from datetime import datetime
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
 
 class FileStorage:
