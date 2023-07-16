f5fa46c3bc22c938d2d7df0a07e7b309be83c85a
kamau-maina <mainkamjames@gmail.com>
2023-07-16 15:45:55 -0700

    solved identation error in basemodel

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,7 @@ class BaseModel:
     """
     def __init__(self, *args, **kwargs):
         """Initialises all instances with id and date attributes"""
-       if kwargs:
+        if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
