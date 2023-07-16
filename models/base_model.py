a922b061dd556130aa895bd255dd0204ece6ebd4
Jane Fadhili Njaga <111033901+fadhilijane@users.noreply.github.com>
2023-07-16 23:40:41 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -17,39 +17,36 @@ class BaseModel:
         """Initialises all instances with id and date attributes"""
        if kwargs:
             for key, value in kwargs.items():
-                if key == '__class__':
-                    continue
-                elif key == 'updated_at':
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                elif key == 'created_at':
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if 'id' not in kwargs.keys():
-                    self.id = str(uuid4())
-                if 'created_at' not in kwargs.keys():
-                    self.created_at = datetime.now()
-                if 'updated_at' not in kwargs.keys():
-                    self.updated_at = datetime.now()
-                setattr(self, key, value)
-      else:
+                if key != "__class__":
+                    setattr(self, key, value)
+            _format = "%Y-%m-%dT%H:%M:%S.%f"
+            self.created_at = datetime.strptime(self.created_at, _format)
+            self.updated_at = datetime.strptime(self.updated_at, _format)
+        else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
-        """ String """
-        return('[' + type(self).__name__ + '] (' + str(self.id) +
-               ') ' + str(self.__dict__))
+        """Prints out specified attributes of an instance"""
+        classname = type(self).__name__
+        iid = self.id
+        i_dic = self.__dict__
+        return "[{}] ({}) {}".format(classname, iid, i_dic)
 
     def save(self):
-        """ save function """
+        """updates the `updated_at` attribute with current datetime"""
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
-        """ Return a dictonary """
-        aux_dict = self.__dict__.copy()
-        aux_dict['__class__'] = self.__class__.__name__
-        aux_dict['created_at'] = self.created_at.isoformat()
-        aux_dict['updated_at'] = self.updated_at.isoformat()
-        return aux_dict
+        """returns a dictionary with all key/values of __dict__"""
+        new_dict = {}
+        for key, value in self.__dict__.items():
+            if key == "created_at" or key == "updated_at":
+                new_dict[key] = value.isoformat()
+            else:
+                new_dict[key] = value
+        new_dict["__class__"] = type(self).__name__
+        return new_dict
